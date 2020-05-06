98f2a145e25d0e08cfa1e481726d804ebf52251a
JulienPetit-1 <56109194+JulienPetit-1@users.noreply.github.com>
2020-05-06 15:10:36 +0200

    Correction TP2
    Add logger, change PUT status and improve display

--- src/main/java/com/esme/Music/application/MusicController.java
+++ src/main/java/com/esme/Music/application/MusicController.java
@@ -1,5 +1,6 @@
 package com.esme.Music.application;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 
 //Rest Controller
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class MusicController {
@@ -27,25 +29,28 @@ public class MusicController {
     @RequestMapping(value = "/musics", method = RequestMethod.POST)
     public ResponseEntity<String> addMusics(String name) {
         this.musics.add(name);
-        return new ResponseEntity<String>("Response from POST", HttpStatus.OK);
+        log.info("Artist created :" + name);
+        return new ResponseEntity<String>("Artist created from POST : " + name, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/musics/{name}", method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteMusics(@PathVariable String name) {
         this.musics.remove(name);
-        return new ResponseEntity<String>("Response from DELETE", HttpStatus.OK);
+        log.info("animal with name :" + name + " is deleted");
+        return new ResponseEntity<String>("Artist deleted from DELETE : " + name, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/musics", method = RequestMethod.PUT)
     public ResponseEntity<String> putMusics(@RequestBody ArrayList<String> musics) {
         this.musics = musics;
-        return new ResponseEntity<String>("Response from PUT", HttpStatus.OK);
+        return new ResponseEntity<String>("List changed from PUT : " + musics, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/musics/{name}", method = RequestMethod.PATCH)
-    public ResponseEntity<String> patchMusics(@PathVariable String name,  String newname) {
+    public ResponseEntity<String> patchMusics(@PathVariable String name, String newname) {
         this.musics.set(this.musics.indexOf(name), newname);
-        return new ResponseEntity<String>("Response from PATCH", HttpStatus.OK);
+        log.info("animal with id :" + name + " is now :" + newname);
+        return new ResponseEntity<String>("Artist "+ name + " replace by " + newname + " from PATCH ", HttpStatus.OK);
     }
 }
 
