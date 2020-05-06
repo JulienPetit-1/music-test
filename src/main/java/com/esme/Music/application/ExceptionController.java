83a1f6fd051596f348511fccf5ae1d8d35dc6104
JulienPetit-1 <56109194+JulienPetit-1@users.noreply.github.com>
2020-05-06 17:45:12 +0200

    Update Controllers
    Update Exception & Return Codes controllers and add some comments

--- src/main/java/com/esme/Music/application/ExceptionController.java
+++ src/main/java/com/esme/Music/application/ExceptionController.java
@@ -9,39 +9,45 @@ import java.util.ArrayList;
 // Exception Controller
 
 @RestController
-@RequestMapping("/api/exceptions")
+@RequestMapping("/api/v2")
 public class ExceptionController {
     private ArrayList<String> musics = new ArrayList();
 
     public ExceptionController() {
-        this.musics.add("Travis Scott");
-        this.musics.add("Pink Floyd");
-        this.musics.add("PNL");
+        this.musics.add("v2Travis Scott"); //On rajoute un préfixe v2 pour vérifier les résultats dans PostMan
+        this.musics.add("v2Pink Floyd");
+        this.musics.add("v2PNL");
     }
 
+//Méthode GET
     @RequestMapping(value = "/musics", method = RequestMethod.GET)
     public ResponseEntity<ArrayList<String>> getMusics() {
-        throw new NullPointerException("Server error");
+        //return new ResponseEntity<ArrayList<String>>(this.musics, HttpStatus.OK); Ancien Code
+        throw new NullPointerException("Server error"); //Cette fois ci, on renvoie une exception
     }
 
+//Méthode POST
     @RequestMapping(value = "/musics", method = RequestMethod.POST)
     public ResponseEntity<String> addMusics(String name) throws Exception {
         this.musics.add(name);
         throw new Exception("Internal server error");
     }
 
+//Méthode DELETE
     @RequestMapping(value = "/musics/{name}", method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteMusics(@PathVariable String name) throws Exception {
         this.musics.remove(name);
         throw new Exception("Ressource not found");
     }
 
+//Méthode PUT
     @RequestMapping(value = "/musics", method = RequestMethod.PUT)
     public ResponseEntity<String> putMusics(@RequestBody ArrayList<String> musics) {
         this.musics = musics;
         return new ResponseEntity<String>("Response from PUT", HttpStatus.OK);
     }
 
+//Méthode PATCH
     @RequestMapping(value = "/musics/{name}", method = RequestMethod.PATCH)
     public ResponseEntity<String> patchMusics(@PathVariable String name,  String newname) {
         this.musics.set(this.musics.indexOf(name), newname);
--- src/main/java/com/esme/Music/application/MusicController.java
+++ src/main/java/com/esme/Music/application/MusicController.java
@@ -19,29 +19,34 @@ public class MusicController {
         this.musics.add("PNL");
     }
 
+//Méthode GET
     @RequestMapping(value = "/musics", method = RequestMethod.GET)
     public ResponseEntity<ArrayList<String>> getMusics() {
         return new ResponseEntity<ArrayList<String>>(this.musics, HttpStatus.OK);
     }
 
+//Méthode POST
     @RequestMapping(value = "/musics", method = RequestMethod.POST)
     public ResponseEntity<String> addMusics(String name) {
         this.musics.add(name);
         return new ResponseEntity<String>("Response from POST", HttpStatus.OK);
     }
 
+//Méthode DELETE
     @RequestMapping(value = "/musics/{name}", method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteMusics(@PathVariable String name) {
         this.musics.remove(name);
         return new ResponseEntity<String>("Response from DELETE", HttpStatus.OK);
     }
 
+//Méthode PUT
     @RequestMapping(value = "/musics", method = RequestMethod.PUT)
     public ResponseEntity<String> putMusics(@RequestBody ArrayList<String> musics) {
         this.musics = musics;
         return new ResponseEntity<String>("Response from PUT", HttpStatus.OK);
     }
 
+//Méthode PATCH
     @RequestMapping(value = "/musics/{name}", method = RequestMethod.PATCH)
     public ResponseEntity<String> patchMusics(@PathVariable String name,  String newname) {
         this.musics.set(this.musics.indexOf(name), newname);
